c40a94197432dc4b7ec8d410f63329b2b49a7de5
Krish <krishtayal29b@gmail.com>
2026-07-28 17:02:32 +0530

    Solved: Search in Rotated Sorted Array II (#81)

---
+++ problems/0081-search-in-rotated-sorted-array-ii/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool search(vector<int>& arr, int target) {
+     int n = arr.size();
+     int low = 0;
+     int high = n - 1;
+     while(low <= high){
+        int mid = (low + high)/2;
+        if(arr[mid] == target) return true;
+        if(arr[mid] >= arr[low]){
+            if((arr[low] <= target) && (arr[mid] >= target)) high = mid - 1;
+            else low = mid + 1;
+        }
+        else{
+            if((arr[mid] <= target) && (arr[high] >= target)) low = mid + 1;
+            else high = mid - 1; 
+        }
+     }
+     return false;
+    }
+};

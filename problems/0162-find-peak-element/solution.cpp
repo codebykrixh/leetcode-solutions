291c2bfe946bce6332334e3da58e5d66753bfcf0
Krish <krishtayal29b@gmail.com>
2026-07-30 13:55:54 +0530

    Solved: Find Peak Element (#162)

--- problems/0162-find-peak-element/solution.cpp
+++ problems/0162-find-peak-element/solution.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
+        if(n==1) return 0;
         if(nums[0] > nums[1]) return 0;
         if(nums[n-1] > nums[n-2]) return (n-1);
         int low = 1, high = n-2;

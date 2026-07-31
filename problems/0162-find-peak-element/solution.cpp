c25e0d870c95e0a26eb45ec64dda91165d8d6836
Krish <krishtayal29b@gmail.com>
2026-07-31 14:50:20 +0530

    Solved: Find Peak Element (#162)

--- problems/0162-find-peak-element/solution.cpp
+++ problems/0162-find-peak-element/solution.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n = nums.size();
+        long long n = nums.size();
         if(n==1) return 0;
         if(nums[0] > nums[1]) return 0;
         if(nums[n-1] > nums[n-2]) return (n-1);
-        int low = 1, high = n-2;
+        long long low = 1, high = n-2;
         while(low <= high){
-            int mid = (low + high)/2;
+            long long mid = (low + high)/2;
             if((nums[mid] > nums[mid - 1]) && (nums[mid] > nums[mid + 1])) return mid;
             if(nums[mid] > nums[mid - 1]) low = mid + 1;
-            else if((nums[mid] < nums[mid - 1]) && (nums[mid] < nums[mid + 1])) low = mid - 1;
+            else if((nums[mid] < nums[mid - 1]) && (nums[mid] < nums[mid + 1])) low = mid + 1;
             else if(nums[mid] < nums[mid - 1]) high = mid - 1;
             
         }

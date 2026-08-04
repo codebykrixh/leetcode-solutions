3e910d8f6b084f497c6d2d07b0b6b82737955ad7
Krish <krishtayal29b@gmail.com>
2026-08-04 23:23:18 +0530

    Solved: Sqrt(x) (#69)

--- problems/0069-sqrtx/solution.cpp
+++ problems/0069-sqrtx/solution.cpp
@@ -4,9 +4,9 @@ public:
         int low = 0, high = x;
         while(low <= high){
             int mid = (low + high)/2;
-            if((mid*mid) == x) return mid;
-            else if(((mid*mid) < x) && ((mid + 1)*(mid + 1) > x)) return mid;
-            else if((mid*mid) < x) low = mid + 1;
+            if((long long)mid*mid == x) return mid;
+            else if(((long long)mid*mid < x) && ((long long)(mid + 1)*(mid + 1) > x)) return mid;
+            else if((long long)mid*mid < x) low = mid + 1;
             else high = mid - 1;
         }
         return -1;

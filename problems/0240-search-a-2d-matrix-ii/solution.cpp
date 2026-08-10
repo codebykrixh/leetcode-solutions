5dcffdcd93333a3f4d385fe5a51be40c21bfb1c3
Krish <krishtayal29b@gmail.com>
2026-08-10 20:44:11 +0530

    Solved: Search a 2D Matrix II (#240)

--- problems/0240-search-a-2d-matrix-ii/solution.cpp
+++ problems/0240-search-a-2d-matrix-ii/solution.cpp
@@ -5,7 +5,7 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
      int row = 0, col = m - 1;
-     while(row < m && col >= 0){
+     while(row < n && col >= 0){
         if(matrix[row][col] == target) return true;
         else if(matrix[row][col] < target) row++;
         else col--;

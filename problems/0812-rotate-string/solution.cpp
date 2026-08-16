fbc42f1584ed85f223fc59737580e9f9cc6313ed
Krish <krishtayal29b@gmail.com>
2026-08-16 18:41:46 +0530

    Solved: Rotate String (#812)

---
+++ problems/0812-rotate-string/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n = s.length();
+        int ind = -1;
+        for(int i = 0; i < n; i++){
+            if(s[0] == goal[i]){
+                ind = i;
+                break;
+            }
+        }
+        if(ind == -1) return false;
+        for(int i = 0; i< n; i++){
+            if(ind == n) ind = ind - n;
+            if(s[i] != goal[ind]) return false;
+            ind++;
+        }
+        return true;
+    }
+};

f584ead63eefccbf1c66f3cfc4363bc877b7bfa5
Krish <krishtayal29b@gmail.com>
2026-08-16 22:58:22 +0530

    Solved: Isomorphic Strings (#205)

--- problems/0205-isomorphic-strings/solution.cpp
+++ problems/0205-isomorphic-strings/solution.cpp
@@ -4,9 +4,12 @@ public:
         int n = s.length();
         long long k = 5 * 10 * 10 * 10 * 10;
         vector<int> arr(k, 0);
+        vector<int> finish(k,0);
         for(int i = 0; i < n ; i++){
-            if(arr[(int)s[i]] == 0) 
+            if(arr[(int)s[i]] == 0 && finish[(int)t[i]] == 0) {
             arr[(int)s[i]] = (int)t[i];
+            finish[(int)t[i]] = 1;
+            }
             else{
                 if(arr[(int)s[i]] != (int)t[i]) return false;
             }

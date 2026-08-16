a9aaf200dcca30dc9ca641d0eb9bb8f67a2510fd
Krish <krishtayal29b@gmail.com>
2026-08-16 17:50:32 +0530

    Solved: Largest Odd Number in String (#2032)

--- problems/2032-largest-odd-number-in-string/solution.cpp
+++ problems/2032-largest-odd-number-in-string/solution.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     string largestOddNumber(string x) {
-        long long num = stoll(x);
-        while(num > 0 && num%2 == 0){
-        int last = num%10;
-        num = num -  last;
-        num = num/10;
+        // long long num = stoll(x);
+        // while(num > 0 && num%2 == 0){
+        // int last = num%10;
+        // num = num -  last;
+        // num = num/10;
+        // }
+        // if(num == 0) return "";
+        // else return to_string(num);
+        int n = x.size();
+        int i = 0;
+        while(!x.empty() && (x[n-1] - '0') % 2 == 0){
+            x.pop_back();
+            n = x.size();            
         }
-        if(num == 0) return "";
-        else return to_string(num);
+        if(x == "") return "";
+        else return x; 
     }
 };
